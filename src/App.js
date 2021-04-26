4f352746f4e33277043ff2aa3baf8d190745e8eb
blackshovon <blackshovon@gmail.com>
2021-04-27 02:29:38 +0600

    useeffect and usereducer together

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import DataFetching1 from './components/DataFetching1';
+import DataFetching2 from './components/DataFetching2';
 import HookCounter from './components/HookCounter';
 import HookCounter2 from './components/HookCounter2';
 import HookCounter3 from './components/HookCounter3';
 import HookCounter4 from './components/HookCounter4';
 import HookMouse from './components/HookMouse';
+import InterveralHookCounter from './components/InterveralHookCounter';
 import MouseContainer from './components/MouseContainer';
 
 function App() {
@@ -16,9 +19,12 @@ function App() {
       <HookCounter3/>
       <hr/>
       <HookCounter4/>
-      <hr/> */}
-      {/* <HookMouse/> */}
+      <hr/>
+      <HookMouse/>
       <MouseContainer/>
+      <InterveralHookCounter/>
+      <DataFetching1/> */}
+      <DataFetching2/>
     </div>
   );
 }
--- src/components/InterveralHookCounter.js
+++ src/components/InterveralHookCounter.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const InterveralHookCounter = () => {
+    const [count, setCount] = useState(0)
+
+    const tick = () => {
+        setCount(count + 1)
+    }
+
+   useEffect(() => {
+        const interval = setInterval(tick, 1000)
+        return () => {
+           clearInterval(interval) 
+        }
+   }, [count])
+
     return (
         <div>
-            
+            {count}
         </div>
     );
 };
